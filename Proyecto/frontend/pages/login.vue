eef8c5223b2336a321148a69895b755348fa7fc0
Fernando <fernando.perez.c@usach.cl>
2022-12-04 20:18:27 -0300

    xd

--- Proyecto/frontend/pages/login.vue
+++ Proyecto/frontend/pages/login.vue
@@ -3,23 +3,23 @@
         <v-app>
             <v-container>
                 <v-row>
-                    <v-col style="padding-left: 25%; padding-right: 25%;">
+                    <v-col class="text-center" justify="center" style="padding-left: 25%; padding-right: 25%;">
 
-                        <v-card shaped outlined style="margin-top: 35%" elevation="10">
-                            <v-card-title><p style="margin-left: 19%">Sistema de Jerarquización Académica</p></v-card-title>
-                            <v-card-subtitle><LogoUSACH style="width: 75%; margin-left: 12.5%;" class="usach-logo"></LogoUSACH></v-card-subtitle>
+                        <v-card class="text-center" justify="center" shaped outlined style="margin-top: 35%" elevation="10">
+                            <v-card-title><p style="margin-left:19%">Sistema de Jerarquización Académica</p></v-card-title>
+                            <v-card-subtitle><LogoUSACH style="width: 75%;" class="usach-logo"></LogoUSACH></v-card-subtitle>
                             
                             <v-card-text>
                                 <v-form ref="form" v-model="valid" lazy-validation>
                                     <v-divider></v-divider>
-                                    <v-text-field style="margin-left: 25%; margin-right: 25%; margin-top: 5% " v-model="email" :rules="emailRules" label="Correo institucional" required></v-text-field>
+                                    <v-text-field class="text-center" justify="center" style="margin-left: 25%; margin-right: 25%; margin-top: 5% " v-model="email" :rules="emailRules" label="Correo institucional" required></v-text-field>
                                     <v-text-field style="margin-left: 25%; margin-right: 25%" v-model="pass" :rules="passRules" label="Contraseña" required></v-text-field>
 
                                     
                                 </v-form>
                             </v-card-text>
                             
-                            <v-btn style="padding-left:15%; padding-right:15%; margin-left: 28.5%; margin-top: -1%; margin-bottom:5%" :disabled="!valid" class="primary mr-4" @click="validate"> Ingresar </v-btn>
+                            <v-btn style="padding-left:15%; padding-right:15%; margin-left: 2%; margin-top: -1%; margin-bottom:5%" :disabled="!valid" class="primary mr-4" @click="validate"> Ingresar </v-btn>
 
 
                         </v-card>
